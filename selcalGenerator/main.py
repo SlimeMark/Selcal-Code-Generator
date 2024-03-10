4bf4f2d27bc2a778a3b0bc6eb43b427d498a63e8
Hengjie Ma <hengjie_ma@outlook.com>
2024-03-10 22:16:28 +0800

    Fixed: Handling of invalid entries.

--- selcalGenerator/main.py
+++ selcalGenerator/main.py
@@ -2,11 +2,9 @@ import generateSelcal as genSc
 
 while True:
     times = input("How many selcals do you want to generate? ")
-    while True:
-        try:
-            times = int(times)
-            break
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-    for i in range(times):
-        print(genSc.generate_selcal())
+    try:
+        times = int(times)
+        for i in range(times):
+            print(genSc.generate_selcal())
+    except ValueError:
+        print("Invalid input. Please enter a number.")
