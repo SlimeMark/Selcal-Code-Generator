668bb1d0a9a998fc69808ab973998c6fefca9a86
Hengjie Ma <hengjie_ma@outlook.com>
2024-04-18 15:46:41 +0800

    Features: Exit confirmation&other stuffs

--- selcalGenerator/main.py
+++ selcalGenerator/main.py
@@ -1,10 +1,14 @@
 import generateSelcal as genSc
 
 while True:
-    times = input("How many selcals do you want to generate? ")
     try:
+        times = input("How many selcals do you want to generate? \nCtrl+C to interrupt.\n\033[36m>>>\033[0m")
         times = int(times)
         for i in range(times):
-            print(genSc.generate_selcal())
+            print("\033[32m"+genSc.generate_selcal()+"\033[0m")
+    except KeyboardInterrupt:
+        print("\n\033[33mConfirm? (y/n)\033[0m")
+        if input().lower() == "y":
+            break
     except ValueError:
-        print("Invalid input. Please enter a number.")
+        print("\033[31mInvalid input. Please enter a number.\n\033[0m")
